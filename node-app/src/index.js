303c0841198f4ec0ae14bf4e37899f15a26ae07c
Kusay.Ahmad <Kusay.ahmad@web.de>
2024-05-27 23:00:54 +0200

    #13 - Docker with MongoDB & NodeJS - Updated MongoDB connection settings in index.js and added configuration for MongoDB in the yml file

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -1,9 +1,20 @@
 const express = require('express'); //This line imports the Express.js framework, which simplifies the process of building web applications and APIs in Node.js.
-
+const mongoose = require('mongoose');
 
 //init app
 const PORT = process.env.PORT || 4000; // run i n aspecifec Port - This variable is set to 4000, indicating the port number on which the server will listen for incoming requests.
 const app = express(); // It initializes an instance of the Express application.
+
+// donnect db
+const DB_USER = 'root';
+const DB_PASSWORD = 'root';
+const DB_PORT = 27017;
+const DB_HOST = '172.27.0.2'; // got it from docker inspect node-app-mongo-1
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+.connect(URI)
+.then (() => console.log('!!! Connected to DB !!!'))
+.catch((err) => console.log('!!! faild to connect to DB!!! : ',err));
 
 //Defining a Route:
 /*This code defines a route for handling GET requests to the root path ('/'). When a user accesses the root path, the server responds by sending the HTML content <h1>...</h1>.
